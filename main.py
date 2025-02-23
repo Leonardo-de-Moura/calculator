07b79ede0606c1b0fa407404379e9a4814949829
Leonardo-de-Moura <francisco.tico.fssm@gmail.com>
2025-02-23 09:21:01 -0300

    up

--- main.py
+++ main.py
@@ -1,5 +1,43 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 main= Flask(__name__)
+CORS(main)
 
-main.run(host="localhost")
+@main.route('/calcular', methods=['POST'])
+def calcular():
+	data= request.get_json()
+	n1= data.get("n1")
+	n2= data.get("n2")
+	operation = data.get("operation")
+
+	if not all([n1,n2,operation]):
+		return "Erro: preencha os campos"
+
+	try:
+		n1,n2, = float(n1), float(n2)
+	except ValueError:
+		return "Erro: os valores nao sao numeros validos"
+
+	match operation:
+		case "/":
+			result= n1/n2
+
+		case "*":
+			result= n1*n2
+		
+		case "+":
+			result= n1+n2
+
+		case "-":
+			result = n1-n2
+		
+		case _:
+			return("Erro: operaçao invalida")
+	
+	return jsonify({"n1": n1, "n2": n2, "operation": operation, "result": result})
+
+
+if __name__ == '__main__':
+    main.run(debug=True)
+			
